40c5f0a80e02c59ca0a414d7bdf9b5b268de7155
Kraos997 <koty5553@gmail.com>
2024-03-19 23:06:32 +0100

    idk

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Utils
+{
+    public static int ArenaX = 20;
+    public static Enemy GetClosest(Vector2 position, float maxRange)
+    {
+        Enemy closest = null;
+        if (Enemy.EnemyList != null)
+        {
+            foreach (Enemy enemy in Enemy.EnemyList)
+            {
+                if (Vector2.Distance(position, enemy.GetPosition()) <= maxRange)
+                {
+                    if (closest == null)
+                    {
+                        closest = enemy;
+                    }
+                    else
+                    {
+                        if (Vector2.Distance(position, enemy.GetPosition()) < Vector2.Distance(position, closest.GetPosition()))
+                        {
+                            closest = enemy;
+                        }
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+}
